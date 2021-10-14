5240aee45c963d6d8894b9247ca98a6cfba2bd41
havva polatlı <74872560+xsstar@users.noreply.github.com>
2021-10-14 23:42:51 +0300

    Lesson 04

---
+++ routes/courseRoute.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const courseController = require('../controllers/courseController');
+
+const router = express.Router();
+
+router.route('/').post(courseController.createCourse); // http://localhost:3000/courses den geldik../ la karşılaştık..olduğu gibi bu fonk.  çalışacak!
+/* router.route('/yeni').post(courseController.yenikurs); // http://localhost:3000/courses/yeni adres olmuş oluyor. */
+
+
+module.exports = router;
