4fd3b6aef7260357bc8d78b1102d97db31f8223c
havva polatlı <74872560+xsstar@users.noreply.github.com>
2021-10-15 16:57:31 +0300

    Lesson 06

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -1,17 +1,53 @@
 const Course = require('../models/Course');
 
+//KURS OLUŞTURMA
 exports.createCourse = async (req, res) => {
-  const course = await Course.create(req.body);
-
   try {
+    const course = await Course.create(req.body);
+
+    // res.send('YENİ KURS OLUŞTURULDU')
     res.status(201).json({
       status: 'success',
       course,
     });
-  } catch {
+  } catch (error) {
     res.status(400).json({
       status: 'fail',
       error,
     });
   }
 };
+
+//OLUŞTURULAN KURSLARI SIRALAMA
+
+exports.getAllCourses = async (req, res) => {
+  try {
+    const courses = await Course.find();
+
+    res.status(200).render('courses', {
+      courses,
+      page_name: 'courses',
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: 'fail',
+      error,
+    });
+  }
+};
+
+//for POSTMAN : verilerimiz hazır olmasa bile..
+/* exports.getAllCourses = async (req, res) => {
+  try {
+    const courses = await Course.find();
+    res.status(201).json({
+      status: 'success',
+      courses,
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: 'fail',
+      error,
+    });
+  }
+}; */
--- routes/courseRoute.js
+++ routes/courseRoute.js
@@ -5,6 +5,7 @@ const router = express.Router();
 
 router.route('/').post(courseController.createCourse); // http://localhost:3000/courses den geldik../ la karşılaştık..olduğu gibi bu fonk.  çalışacak!
 /* router.route('/yeni').post(courseController.yenikurs); // http://localhost:3000/courses/yeni adres olmuş oluyor. */
+router.route('/').get(courseController.getAllCourses);
 
 
 module.exports = router;
