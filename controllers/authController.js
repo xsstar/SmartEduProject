5ef8bad9b065c3470dfe6a4612391ed1d55874d0
havva polatlı <74872560+xsstar@users.noreply.github.com>
2021-10-16 17:29:11 +0300

    Lesson 10

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,5 @@
 const User = require('../models/User');
+const bcrypt = require('bcrypt');
 
 exports.createUser = async (req, res) => {
   try {
@@ -14,4 +15,26 @@ exports.createUser = async (req, res) => {
       error,
     });
   }
-}; 
+};
+
+exports.loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    await User.findOne({ email }, (err, user) => {
+      if (user) {
+      return  bcrypt.compare(password, user.password, (err, same) => {
+          if (same) {
+            // USER SESSION
+          return  res.status(200).send('YOU ARE LOGGED IN');
+          }
+        });
+      }
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: 'fail',
+      error,
+    });
+  }
+};
--- controllers/pageController.js
+++ controllers/pageController.js
@@ -15,3 +15,9 @@ exports.getIndexPage = (req, res) => {
       page_name: 'register',
     });
   };
+
+  exports.getLoginPage = (req, res) => {
+    res.status(200).render('login', {
+      page_name: 'login',
+    });
+  };
