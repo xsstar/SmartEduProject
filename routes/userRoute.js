c12c2eab4a79a6fd2c34a86e3a3abae2109ea9b5
havva polatlı <74872560+xsstar@users.noreply.github.com>
2021-10-18 00:05:21 +0300

    Lesson 14

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const authController = require('../controllers/authController');
+const authMiddleware = require('../middlewares/authMiddleware');
 
 const router = express.Router();
 
@@ -7,6 +8,6 @@ router.route('/signup').post(authController.createUser); // http://localhost:300
 router.route('/login').post(authController.loginUser);
 router.route('/logout').get(authController.logoutUser);
 router.route('/logout').get(authController.logoutUser);
-router.route('/dashboard').get(authController.getDashboardPage); http://localhost:3000/users/dashboard
+router.route('/dashboard').get(authMiddleware, authController.getDashboardPage); http://localhost:3000/users/dashboard
 
 module.exports = router;
