1ad77fadccaecf1228b653ab50d187da550506cf
havva polatlı <74872560+xsstar@users.noreply.github.com>
2021-10-21 23:34:09 +0300

    Lesson 26

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -28,6 +28,7 @@ router.route('/signup').post(
 router.route('/login').post(authController.loginUser);
 router.route('/logout').get(authController.logoutUser);
 router.route('/logout').get(authController.logoutUser);
+router.route('/:id').delete(authController.deleteUser);
 router.route('/dashboard').get(authMiddleware, authController.getDashboardPage); http://localhost:3000/users/dashboard
 
 module.exports = router;
